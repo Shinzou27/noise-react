3e486a680b76f2c607a973f7bdb25511c8b862db
Felipe <felipecassianobarbosa@hotmail.com>
2025-05-19 03:21:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div id='navbar' className='navbar-container'>
+      <span>Ruídos na computação gráfica</span>
+    </div>
+    </>
+  )
+}
+
+export default App
